fb96455cd1521a088fb4ca5f274e3cf0bd0aa9fa
oliviercar0n <oliviercaron28@gmail.com>
2023-12-07 01:57:11 -0500

    Format day 1

--- src/day-01.py
+++ src/day-01.py
@@ -1,43 +1,40 @@
-def p1(input_data: str):
-    sum_calibration_values = 0
-    for value in input_data:
-        digits = [c for c in value if c.isdigit()]
-        sum_calibration_values += int(digits[0] + digits[-1])
-
-    print(f"The sum of all configuration values is {sum_calibration_values}")
-
-def p2(input_data: str):
-
-    numbers = [
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-    ]
-
-    sum_calibration_values = 0
-    for value in input_data:
-        digits = []
-        for i, character in enumerate(value):
-            if character.isdigit():
-                digits.append(character)
-            for d, number in enumerate(numbers):
-                if value[i:].startswith(number):
-                    digits.append(str(d))
-
-        sum_calibration_values += int(digits[0] + digits[-1])
-
-    print(f"The sum of all configuration values is {sum_calibration_values}")
-
-
 with open("day-01-puzzle-input.txt", "r") as f:
     calibration_data_input = f.readlines()
 
-p1(calibration_data_input)
-p2(calibration_data_input)
+# Part 1
+
+sum_calibration_values = 0
+for value in calibration_data_input:
+    digits = [c for c in value if c.isdigit()]
+    sum_calibration_values += int(digits[0] + digits[-1])
+
+print(f"The sum of all configuration values is {sum_calibration_values}")
+
+# Part 2
+
+numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+sum_calibration_values = 0
+for value in calibration_data_input:
+    digits = []
+    for i, character in enumerate(value):
+        if character.isdigit():
+            digits.append(character)
+        for d, number in enumerate(numbers):
+            if value[i:].startswith(number):
+                digits.append(str(d))
+
+    sum_calibration_values += int(digits[0] + digits[-1])
+
+print(f"The sum of all configuration values is {sum_calibration_values}")
